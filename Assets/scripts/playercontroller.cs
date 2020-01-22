bf2d3cd42897656af95d4ec9741ebcfd34f7db15
sbderosa <sbderosa@ncsu.edu>
2020-01-22 13:58:09 -0500

    Initial commit of project

---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+
+
+    public int numBalls; 
+    public float speed; 
+    private Rigidbody rb;
+
+    void Start()
+    {
+        numBalls = 5;
+        rb = GetComponent<Rigidbody>();
+        if (gameObject.name.Contains("Clone"))
+        {
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+        }
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && numBalls > 0 && !gameObject.name.Contains("Clone")) 
+        {
+            Rigidbody clone;
+            clone = Instantiate(rb, transform.position, transform.rotation);
+            Vector3 movement = new Vector3(0, 0.0f, 0);
+
+            rb.AddForce(movement * speed);
+
+            clone.velocity = transform.TransformDirection(Vector3.forward);
+            numBalls--;
+        }
+       
+    }
+
+    //public class clone
+    //{
+    //    private Rigidbody rb;
+    //    void Start()
+    //    {
+
+    //        rb = GetComponent<Rigidbody>();
+    //        rb.constraints = RigidbodyConstraints.FreezePositionY;
+    //    }
+    //}
+}
---
+++ Assets/scripts/spurcontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spurcontroller : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+    }
+
+    // Update is called once per frame
+    void OnCollision(Collider other)
+    {
+        rb.constraints = RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
+        rb.angularVelocity.Set(10, 10, 10);
+    }
+
+}
