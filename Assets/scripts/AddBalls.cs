e7a929344130dbc5b8db28ba33366363d0901b41
Sam Weaver <sam@samweaver.com>
2020-01-28 21:40:00 -0500

    Save UI logic and ball triggers

---
+++ Assets/scripts/AddBalls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBalls : MonoBehaviour
+{
+    // set a value to this in the UI
+    public playercontroller playerController;
+
+    // number of balls awarded (set in UI)
+    public int numBalls = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // When a ball enters
+    void OnTriggerEnter(Collider other) {
+        if (other.name.Contains("playercontroller")) {
+            Destroy(other.gameObject);
+            playercontroller.numBalls += numBalls;
+        }
+    }
+}
--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playercontroller : MonoBehaviour
 {
 
-    public static int numBalls = 50;
+    public static int numBalls = 5;
     public bool isClone = false;
     public float speed = 5; 
     public Rigidbody rb;
     public SphereCollider collider;
     public MeshRenderer renderer;
+    public Text textbox;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         collider = GetComponent<SphereCollider>();
         renderer = GetComponent<MeshRenderer>();
+        
         if (gameObject.name.Contains("Clone")) isClone = true;
         if (isClone) {
             Vector3 movement = new Vector3(Random.Range(-250f, 250f), -500f, 0);
@@ -35,6 +38,8 @@ public class playercontroller : MonoBehaviour
             // clone.velocity = transform.TransformDirection(Vector3.forward);
             numBalls--;
         }
+
+        textbox.text = ("You have " + numBalls + " balls left!");
     }
 
     void FixedUpdate()
