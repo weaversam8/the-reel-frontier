c0b4fb8d0b7070be4916cbd55846a2b21584ff8b
sbderosa <sbderosa@ncsu.edu>
2020-01-28 21:56:47 -0500

    added sound effects

---
+++ Assets/scripts/pegscript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pegscript : MonoBehaviour
+{
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.name.Contains("playercontroller"))
+        {
+
+            audioSource.PlayOneShot(audioSource.clip);
+            //audioSource.PlayOneShot(audioSource.clip);
+
+        }
+        
+    }
+}
--- Assets/scripts/wallsscript.cs
+++ Assets/scripts/wallsscript.cs
@@ -2,13 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ball_delete : MonoBehaviour
+public class wallsscript : MonoBehaviour
 {
-
-    // set a value to this in the UI
-    public playercontroller playerController;
     public AudioSource audioSource; 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +12,15 @@ public class ball_delete : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision other)
     {
-        
-    }
+        if (other.collider.name.Contains("playercontroller"))
+        {
 
-    // When a ball enters
-    void OnTriggerEnter(Collider other) {
-        if (other.name.Contains("playercontroller")) {
-            Destroy(other.gameObject);
-            playercontroller.numBalls++;
             audioSource.PlayOneShot(audioSource.clip);
-            
+            //audioSource.PlayOneShot(audioSource.clip);
+
         }
+
     }
 }
